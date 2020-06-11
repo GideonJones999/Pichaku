c66ea7ecd56998833fa13a9713f88838883acf40
Gideon Jones <gideon.w.jones@gmail.com>
2020-06-11 11:05:34 -0600

    Init Commit

---
+++ Pichaku/Pichaku/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+	cout << "^   ^" << endl << "(*-*) /" << endl << "(u-u)N" << endl;
+	system("PAUSE");
+	return 0;
+}
+
+string write() {
+	return "0";
+}
